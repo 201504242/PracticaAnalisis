2edc5ad3682022fcc7d044a45b444f23bb72281c
201504242 <201504242@ingenieria.usac.edu.gt>
2019-03-18 13:52:09 -0600

    Agregando funcion buscar Cancion

--- playlist.php
+++ playlist.php
@@ -27,3 +27,27 @@
 	<script src="script.js"></script>
 </body>
 </html>
+
+-- En esta parte estara el metodo para buscar cancion
+<video controls>
+  <source src="dynamicsearch.mp4" type="video/mp4"></source>
+  <a href="dynamicsearch.mp4">
+    <img src="dynamicsearch.jpg" alt="App de b&uaacute;squeda din&aacute;mica en Firefox OS">
+  </a>
+  <p>Clic en la im&aacute;gen para reproducir un v&iacute;deo demo de la app de b&uaacute;squeda din&aacute;mica</p>
+</video>
+
+<video>
+<source id="mp4_src"
+  src="video.mp4"
+  type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+</source>
+<source id="3gp_src"
+  src="video.3gp"
+  type='video/3gpp; codecs="mp4v.20.8, samr"'>
+</source>
+<source id="ogg_src"
+  src="video.ogv"
+  type='video/ogg; codecs="theora, vorbis"'>
+</source>
+</video>
