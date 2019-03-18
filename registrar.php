e68947b1b56358b78e735a7c82608907d0163a40
fredyramirez <fredyramirezsando@gmail.com>
2019-03-18 12:44:15 -0600

    Registrar Usuario

---
+++ registrar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Formulario</title>
+    <meta charset="utf-8">
+    <link type="text/css" href="./css/style.css" rel="stylesheet" />
+</head>
+ 
+<body>
+ 
+    <div id="registrar">
+<a href="./php/registro.php"</a>Registrarse</a>
+</div> <!-- fin opcion-->
+ 
+    <div id="envoltura">
+        <div id="contenedor">
+ 
+            <div id="cabecera" >
+                <img src="./css/images/logo.gif">
+            </div>
+             <div id="pie">Sistema de Login Y Registro</div>
+        </div><!-- fin contenedor -->
+    </div><!--fin envoltura-->
+</body>
